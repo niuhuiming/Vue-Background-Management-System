8855d13885c7b1e6749452552f67eb7078e3049d
nhm <3059350051@qq.com>
2022-05-07 20:48:44 +0800

    项目框架

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Container, Main, Header, Aside, Menu, Submenu, MenuItem, MenuItemGroup, Dropdown, DropdownMenu, DropdownItem, Row, Col, Card, Table, TableColumn } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/less/index.less'
+
+import router from '../router'
+import store from '../store'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Main)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
+Vue.use(Table)
+Vue.use(TableColumn)
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
